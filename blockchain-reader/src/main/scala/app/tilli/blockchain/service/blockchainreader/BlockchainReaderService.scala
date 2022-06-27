0478c4e9a1e3510a5e6ddb52b8add39c93760fa6
Anders Schwartz <>
2022-06-27 09:17:44 -0400

    alpha-3: read from bucket

--- blockchain-reader/src/main/scala/app/tilli/blockchain/service/blockchainreader/BlockchainReaderService.scala
+++ blockchain-reader/src/main/scala/app/tilli/blockchain/service/blockchainreader/BlockchainReaderService.scala
@@ -22,10 +22,10 @@ object BlockchainReaderService extends IOApp {
 
     val sslConfig = Map(
       "security.protocol" -> "SSL",
-      "ssl.truststore.location" -> "/Users/andersschwartz/code/data/aiven/client.truststore-14236929421944531078.jks",
+      "ssl.truststore.location" -> "gs://tilli-prod-kafka-secrets/client.truststore-14236929421944531078.jks",
       "ssl.truststore.password" -> "a1cd60a4e89d436c913dc996bf40d6ca",
       "ssl.keystore.type" -> "PKCS12",
-      "ssl.keystore.location" -> "/Users/andersschwartz/code/data/aiven/client.keystore-12010477626053255492.p12",
+      "ssl.keystore.location" -> "gs://tilli-prod-kafka-secrets/client.keystore-12010477626053255492.p12",
       "ssl.keystore.password" -> "fd13542854dd47d7bbfb774b32caf261",
       "ssl.key.password" -> "fd13542854dd47d7bbfb774b32caf261",
       "ssl.endpoint.identification.algorithm" -> "",
